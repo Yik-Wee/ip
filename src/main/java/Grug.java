98fb88d0cb79a141685f3f467b270670d9bbe12c
Yik Wee <77543794+Yik-Wee@users.noreply.github.com>
2026-08-15 21:38:57 +0800

    Rename chatbot to Grug

---
+++ src/main/java/Grug.java
@@ -0,0 +1,18 @@
+/**
+ * Entrypoint of the Grug chatbot application
+ */
+public class Grug {
+    public static void main(String[] args) {
+        String banner = "  ▄████  ██▀███   █    ██   ▄████ \n"
+                + " ██▒ ▀█▒▓██ ▒ ██▒ ██  ▓██▒ ██▒ ▀█▒\n"
+                + "▒██░▄▄▄░▓██ ░▄█ ▒▓██  ▒██░▒██░▄▄▄░\n"
+                + "░▓█  ██▓▒██▀▀█▄  ▓▓█  ░██░░▓█  ██▓\n"
+                + "░▒▓███▀▒░██▓ ▒██▒▒▒█████▓ ░▒▓███▀▒\n"
+                + " ░▒   ▒ ░ ▒▓ ░▒▓░░▒▓▒ ▒ ▒  ░▒   ▒ \n"
+                + "  ░   ░   ░▒ ░ ▒░░░▒░ ░ ░   ░   ░ \n"
+                + "░ ░   ░   ░░   ░  ░░░ ░ ░ ░ ░   ░ \n"
+                + "      ░    ░        ░           ░ \n";
+
+        System.out.println(banner);
+    }
+}

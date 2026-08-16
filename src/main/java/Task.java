572732150ef55ecede93bb00cfca98c1c6fd4d11
Yik Wee <77543794+Yik-Wee@users.noreply.github.com>
2026-08-16 22:24:38 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,52 @@
+/**
+ * Represents a task, storing the task `details` and whether the task has been
+ * completed or not.
+ */
+public class Task {
+    private String details;
+    private boolean isCompleted;
+
+    /**
+     * Creates a new *incomplete* `Task`.
+     *
+     * @param details the details of the task.
+     */
+    public Task(String details) {
+        this(details, false);
+    }
+
+    /**
+     * Creates a new `Task`.
+     *
+     * @param details     the details of the task.
+     * @param isCompleted whether the task has been completed.
+     */
+    public Task(String details, boolean isCompleted) {
+        this.details = details;
+        this.isCompleted = isCompleted;
+    }
+
+    /**
+     * Marks the task as complete.
+     */
+    public void markComplete() {
+        this.isCompleted = true;
+    }
+
+    /**
+     * Marks the task as incomplete.
+     */
+    public void markIncomplete() {
+        this.isCompleted = false;
+    }
+
+    public String getDetails() {
+        return this.details;
+    }
+
+    @Override
+    public String toString() {
+        String checkbox = this.isCompleted ? "[X]" : "[ ]";
+        return "%s %s".formatted(checkbox, details);
+    }
+}

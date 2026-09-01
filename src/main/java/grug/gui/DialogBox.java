1950dec41090944cf248cab7add278c42c74a5a2
Yik Wee <77543794+Yik-Wee@users.noreply.github.com>
2026-09-01 17:10:08 +0800

    Fix minor javadoc mistakes

--- src/main/java/grug/gui/DialogBox.java
+++ src/main/java/grug/gui/DialogBox.java
@@ -10,8 +10,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 
 /**
- * Represents a dialog box consisting of an ImageView to represent the speaker's
- * face and a label containing text from the speaker.
+ * Represents a dialog box consisting of a label containing text from the
+ * speaker.
  */
 public class DialogBox extends HBox {
     @FXML
